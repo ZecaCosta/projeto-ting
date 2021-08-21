a718d0ab11445dc02396fc21226ef8dec9103328
Zeca Costa <jccostaso@gmail.com>
2021-08-21 19:04:16 -0300

    feature: req3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,24 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    duplicate_file = False
+    instance_length = instance.__len__()
+    for item in range(instance_length):
+        searched_dic = instance.search(item)
+        if path_file == searched_dic["nome_do_arquivo"]:
+            duplicate_file = True
+
+    if not duplicate_file:
+        lines_list = txt_importer(path_file)
+        insert_dic = {
+                "nome_do_arquivo": path_file,
+                "qtd_linhas": len(lines_list),
+                "linhas_do_arquivo": lines_list
+        }
+        instance.enqueue(insert_dic)
+        sys.stdout.write(f'{insert_dic}\n')
 
 
 def remove(instance):
