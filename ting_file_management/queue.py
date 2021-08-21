801edb929d014a64d4f4347b4c4d75dc57deb00c
Zeca Costa <jccostaso@gmail.com>
2021-08-21 11:00:57 -0300

    feature: req1

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,25 @@
 class Queue:
+    FIFO = 0
+
     def __init__(self):
-        """Inicialize sua estrutura"""
+        self._data = []
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self._data)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self._data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if self._data:
+            return self._data.pop(self.FIFO)
+        return None
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        smallest = 0
+        highier = self.__len__() - 1
+        _type = type(index)
+
+        if _type != int or smallest > index < highier:
+            raise IndexError
+        return self._data[index]
