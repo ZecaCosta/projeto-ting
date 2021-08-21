a7dd3e8d73d562c31274947b44fd026635493474
Zeca Costa <jccostaso@gmail.com>
2021-08-21 13:40:35 -0300

    feature: req2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith('.txt'):
+        sys.stderr.write('Formato inválido\n')
+
+    try:
+        with open(path_file, 'r') as file:
+            file_content = file.read()
+            return file_content.split('\n')
+
+    except FileNotFoundError:
+        sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
