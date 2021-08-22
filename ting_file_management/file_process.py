afe19118c2265ecefe965ba49d20e500a9565dc6
Zeca Costa <jccostaso@gmail.com>
2021-08-22 11:07:37 -0300

    feature: req5

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -7,15 +7,15 @@ def process(path_file, instance):
     instance_length = instance.__len__()
     for item in range(instance_length):
         searched_dic = instance.search(item)
-        if path_file == searched_dic["nome_do_arquivo"]:
+        if path_file == searched_dic['nome_do_arquivo']:
             duplicate_file = True
 
     if not duplicate_file:
         lines_list = txt_importer(path_file)
         insert_dic = {
-                "nome_do_arquivo": path_file,
-                "qtd_linhas": len(lines_list),
-                "linhas_do_arquivo": lines_list
+                'nome_do_arquivo': path_file,
+                'qtd_linhas': len(lines_list),
+                'linhas_do_arquivo': lines_list
         }
         instance.enqueue(insert_dic)
         sys.stdout.write(f'{insert_dic}\n')
@@ -27,9 +27,14 @@ def remove(instance):
         return sys.stdout.write('Não há elementos\n')
 
     removed_dic = instance.dequeue()
-    path_file = removed_dic["nome_do_arquivo"]
+    path_file = removed_dic['nome_do_arquivo']
     sys.stdout.write(f'Arquivo {path_file} removido com sucesso\n')
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        searched_dic = instance.search(position)
+        sys.stdout.write(str(searched_dic))
+
+    except IndexError:
+        sys.stderr.write('Posição inválida\n')
